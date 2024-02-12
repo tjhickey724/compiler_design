cf04d4890e050be09888e61b53e90efa6fbbf566
tim <tjhickey@brandeis.edu>
2024-02-12 11:17:00 -0500

    cleaned up Trig code

--- code/Calculators/build.sh
+++ code/Calculators/build.sh
@@ -1,2 +1,8 @@
 java -classpath ../javacc.jar javacc Calculator.jj
+java -classpath ../javacc.jar javacc CalculatorTrig.jj
+java -classpath ../javacc.jar javacc CalculatorInt.jj
+java -classpath ../javacc.jar javacc CalculatorBigInt.jj
 javac Calculator.java
+javac CalculatorTrig.java
+javac CalculatorInt.java
+javac CalculatorBigInt.java
